9a10d460d2886b2b2a9b4650f8ac6bb646b12b85
yujinchang00 <99487833+yujinchang00@users.noreply.github.com>
2022-12-19 21:22:27 +0900

    Delete backend_api directory

--- backend_api/restapi/Passage/serializers.py
+++
@@ -1,8 +0,0 @@
-from unittest.util import _MAX_LENGTH
-from rest_framework import serializers
-from .models import Passage
-
-class PassageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Passage
-        fields = '__all__'
