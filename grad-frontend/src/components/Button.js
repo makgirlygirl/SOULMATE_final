3cc0f2b021c5a0a23f3a856d3af1d64fdd53b021
Yoonseo Kim <61778930+ottl-seo@users.noreply.github.com>
2022-12-22 17:47:57 +0900

    frontend: update latest code

--- grad-frontend/src/components/Button.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-const Button = ({label, className, onClick}) => {
-    return (
-      <button className={className} onClick={onClick}>{label}</button>
-    ); // 
-  };
-export default Button;
